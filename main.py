6a67fded28991cf96b402934cd8556c2a4b656a3
Предраг Николић <idmpepe@gmail.com>
2023-01-29 01:56:48 +0100

    add EXPERIMENTAL_PHANTOMS variable

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ import sublime_plugin
 from typing import List, Optional
 
 
+EXPERIMENTAL_PHANTOMS = True
+
+#region EXPERIMENTAL_PHANTOMS
+REND_PHANTOM_SET_MAP = {}
+def get_phantom_set(view: sublime.View):
+	global REND_PHANTOM_SET_MAP
+	id = view.buffer_id()
+	phantom_set_key = "rend_phantom_set"
+	phantom_set = REND_PHANTOM_SET_MAP.get(id)
+	if phantom_set is None:
+		REND_PHANTOM_SET_MAP[id] = sublime.PhantomSet(view, phantom_set_key)
+	return REND_PHANTOM_SET_MAP[id]
+#endregion
+
+
 class Marker:
 	def __init__(self, view: sublime.View, region: sublime.Region) -> None:
 		self.view: sublime.View = view
 		self.region = region
-		end_of_first_line = view.line(region.begin()).end()
-		self.fold_region = sublime.Region(end_of_first_line, region.end())
+		#region EXPERIMENTAL_PHANTOMS
+		fold_start = region.begin() if EXPERIMENTAL_PHANTOMS else view.line(region.begin()).end()
+		#endregion
+		self.fold_region = sublime.Region(fold_start, region.end())
 
 	def fold(self):
 		self.view.fold(self.fold_region)
 
 	def unfold(self):
 		self.view.unfold(self.fold_region)
+
+	def is_folded(self):
+		return self.view.is_folded(self.region)
 
 	def is_at_comment(self, point: int):
 		first_line = self.view.line(self.region.begin())
@@ -24,59 +44,116 @@ class Marker:
 	def contains(self, region: sublime.Region):
 		return self.region.contains(region)
 
+	#region EXPERIMENTAL_PHANTOMS
+	def phantom(self):
+		text = self.view.substr(self.view.find(r'\W+.*+\n', self.region.begin())).strip()
+		command = sublime.command_url('rend_unfold', {'point': self.region.begin()})
+		comment_region = self.view.expand_to_scope(self.region.begin(), 'comment')
+		phantom_start = comment_region if comment_region else self.region
+		return sublime.Phantom(
+			phantom_start,
+			f'''<a href="{command}"
+				style="display: block; text-decoration: none; padding: 0 0.4rem; border: 1px solid color(var(--foreground) alpha(0.25)); color: color(var(--foreground) alpha(0.75))">
+				{text}
+			</a>''',
+			sublime.LAYOUT_INLINE
+		)
+	#endregion
+
 
 class FindEndRegions(sublime_plugin.ViewEventListener):
 	def on_load(self) -> None:
 		self.view.run_command('rend_fold_all')
 
-
 class RendFoldAll(sublime_plugin.TextCommand):
 	def run(self, _: sublime.Edit) -> None:
+		#region EXPERIMENTAL_PHANTOMS
+		phantoms: List[sublime.Phantom] = []
+		#endregion
 		marker_regions = find_marked_regions(self.view)
 		markers = marker_regions_to_marker(self.view, marker_regions)
 		for m in markers:
 			m.fold()
+			#region EXPERIMENTAL_PHANTOMS
+			phantoms.append(m.phantom())
+			#endregion
+		#region EXPERIMENTAL_PHANTOMS
+		if EXPERIMENTAL_PHANTOMS:
+			phantom_set = get_phantom_set(self.view)
+			phantom_set.update(phantoms)
+		#endregion
 
 
 class RendUnfoldAll(sublime_plugin.TextCommand):
 	def run(self, _: sublime.Edit) -> None:
+		#region EXPERIMENTAL_PHANTOMS
+		phantoms: List[sublime.Phantom] = []
+		#endregion
 		marker_regions = find_marked_regions(self.view)
 		markers = marker_regions_to_marker(self.view, marker_regions)
 		for m in markers:
 			m.unfold()
-
+		#region EXPERIMENTAL_PHANTOMS
+		if EXPERIMENTAL_PHANTOMS:
+			phantom_set = get_phantom_set(self.view)
+			phantom_set.update([])
+		#endregion
 
 class RendFold(sublime_plugin.TextCommand):
 	def run(self, _: sublime.Edit) -> None:
 		sel = self.view.sel()
 		if not sel:
 			return
+		#region EXPERIMENTAL_PHANTOMS
+		phantoms: List[sublime.Phantom] = []
+		#endregion
 		cursor = sel[0].b
 		marker_regions = find_marked_regions(self.view)
 		markers = marker_regions_to_marker(self.view, marker_regions)
 		for m in markers:
 			if m.is_at_comment(cursor):
 				m.fold()
-				break
+			#region EXPERIMENTAL_PHANTOMS
+			if m.is_folded():
+				phantoms.append(m.phantom())
+			#endregion
+		#region EXPERIMENTAL_PHANTOMS
+		if EXPERIMENTAL_PHANTOMS:
+			phantom_set = get_phantom_set(self.view)
+			phantom_set.update(phantoms)
+		#endregion
 
 
 class RendUnfold(sublime_plugin.TextCommand):
-	def run(self, _: sublime.Edit) -> None:
+	def run(self, _: sublime.Edit, point: Optional[int]=None) -> None:
 		sel = self.view.sel()
 		if not sel:
 			return
-		cursor = sel[0].b
+		#region EXPERIMENTAL_PHANTOMS
+		phantoms: List[sublime.Phantom] = []
+		#endregion
+		cursor = point or sel[0].b
 		marker_regions = find_marked_regions(self.view)
 		markers = marker_regions_to_marker(self.view, marker_regions)
 		for i in range(len(markers)):
 			marker = markers[i]
 			if marker.is_at_comment(cursor):
 				marker.unfold()
+				print('marker', self.view.substr(marker.region))
 				#region But Fold Inner Markers
 				for inner_marker in markers[i+1:]:
 					if marker.contains(inner_marker.region):
 						inner_marker.fold()
-				#endregion
+				# endregion
+			#region EXPERIMENTAL_PHANTOMS
+			if marker.is_folded():
+				phantoms.append(marker.phantom())
+			#endregion
+		#region EXPERIMENTAL_PHANTOMS
+		if EXPERIMENTAL_PHANTOMS:
+			phantom_set = get_phantom_set(self.view)
+			phantom_set.update(phantoms)
+		#endregion
 
 
 def find_marked_regions(view: sublime.View) -> List[sublime.Region]:
